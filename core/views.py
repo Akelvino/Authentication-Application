aac835e0cc7cf29347a29e4fe4e60c9386669671
Akelvino <vinho892@gmail.com>
2025-12-26 16:53:41 +0300

    Finished Signup form

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignupForm
 
 # Create your views here.
-def home(request):
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def register(request):
     form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
     return render(request, 'core/register.html', {'form':form})
